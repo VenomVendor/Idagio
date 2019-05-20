28667cd9aced121c03e4434657a8b9f4b796e1a4
VenomVendor <info@VenomVendor.com>
2019-05-19 22:13:28 -0400

    Added Project files
    Added gitignore to ignore autogenerated files
    Added .idea/* configuration for code style, copyrights, formatting, scopes
    Added autogenerated source files

---
+++ app/src/main/java/com/venomvendor/idagio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.venomvendor.idagio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
